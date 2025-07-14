4d9c2447115f94e50fa36545496576ddba0b9470
kaba4cow <kaba4cow@gmail.com>
2025-07-14 22:20:10 +0300

    Create UserMapper

---
+++ src/main/java/com/kaba4cow/imgxiv/auth/dto/UserMapper.java
@@ -0,0 +1,18 @@
+package com.kaba4cow.imgxiv.auth.dto;
+
+import org.springframework.stereotype.Component;
+
+import com.kaba4cow.imgxiv.domain.user.User;
+
+@Component
+public class UserMapper {
+
+	public UserDto mapToDto(User user) {
+		return new UserDto(//
+				user.getId(), //
+				user.getUsername(), //
+				user.getEmail()//
+		);
+	}
+
+}
