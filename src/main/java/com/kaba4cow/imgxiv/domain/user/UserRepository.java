c979ab8667be2ba5295cd7923fd4365eae2220d4
kaba4cow <kaba4cow@gmail.com>
2025-07-14 21:56:27 +0300

    Add UserRepository#findByUsernameOrEmail

--- src/main/java/com/kaba4cow/imgxiv/domain/user/UserRepository.java
+++ src/main/java/com/kaba4cow/imgxiv/domain/user/UserRepository.java
@@ -1,9 +1,12 @@
 package com.kaba4cow.imgxiv.domain.user;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+
+	Optional<User> findByUsernameOrEmail(String usernameOrEmail);
 
 }
