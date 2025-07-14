9f0da657168693d40075d0e591819e26a8de8b99
kaba4cow <kaba4cow@gmail.com>
2025-07-14 21:10:17 +0300

    Create RegisterRequest

---
+++ src/main/java/com/kaba4cow/imgxiv/auth/dto/RegisterRequest.java
@@ -0,0 +1,20 @@
+package com.kaba4cow.imgxiv.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class RegisterRequest {
+
+	private String username;
+
+	private String email;
+
+	private String password;
+
+}
