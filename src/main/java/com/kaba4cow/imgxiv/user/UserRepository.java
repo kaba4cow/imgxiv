055e00d71f8ff3d0f5be4dffce062a85f17cf3aa
kaba4cow <kaba4cow@gmail.com>
2025-07-14 12:47:46 +0300

    Create UserRepository

---
+++ src/main/java/com/kaba4cow/imgxiv/user/UserRepository.java
@@ -0,0 +1,9 @@
+package com.kaba4cow.imgxiv.user;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+}
