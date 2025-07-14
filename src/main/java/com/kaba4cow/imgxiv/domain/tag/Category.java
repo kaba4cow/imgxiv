1a4a5cbd521b189a18be4008f217b8deecf9e41b
kaba4cow <kaba4cow@gmail.com>
2025-07-14 13:12:41 +0300

    Create Category entity

---
+++ src/main/java/com/kaba4cow/imgxiv/domain/tag/Category.java
@@ -0,0 +1,32 @@
+package com.kaba4cow.imgxiv.domain.tag;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "table_category", uniqueConstraints = @UniqueConstraint(columnNames = "column_name"))
+public class Category {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	private UUID id;
+
+	@Column(name = "column_name")
+	private String name;
+
+}
