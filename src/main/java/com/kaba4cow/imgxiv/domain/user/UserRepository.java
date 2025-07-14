d84c9b841ce38f6f368ca9a6d067346d172b39a9
kaba4cow <kaba4cow@gmail.com>
2025-07-14 23:06:56 +0300

    Fix UserRepository#findByUsernameOrEmail

--- src/main/java/com/kaba4cow/imgxiv/domain/user/UserRepository.java
+++ src/main/java/com/kaba4cow/imgxiv/domain/user/UserRepository.java
@@ -4,10 +4,13 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
 
-	Optional<User> findByUsernameOrEmail(String usernameOrEmail);
+	@Query("SELECT u FROM User u WHERE u.username = :usernameOrEmail OR u.email = :usernameOrEmail")
+	Optional<User> findByUsernameOrEmail(@Param("usernameOrEmail") String usernameOrEmail);
 
 	boolean existsByUsername(String username);
 
