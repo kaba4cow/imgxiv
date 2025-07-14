e2832f8dfb92b35ef2b64b9e6407de53b4027e2b
kaba4cow <kaba4cow@gmail.com>
2025-07-14 23:14:35 +0300

    Add DefaultUserDetailsService

---
+++ src/main/java/com/kaba4cow/imgxiv/auth/service/DefaultUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.kaba4cow.imgxiv.auth.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.kaba4cow.imgxiv.domain.user.UserDetailsAdapter;
+import com.kaba4cow.imgxiv.domain.user.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class DefaultUserDetailsService implements UserDetailsService {
+
+	private final UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		return userRepository.findByUsername(username)//
+				.map(UserDetailsAdapter::of)//
+				.orElseThrow(() -> new UsernameNotFoundException("User not found"));
+	}
+
+}
