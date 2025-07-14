f6afa2981c069955c625fec327f174043d74edfe
kaba4cow <kaba4cow@gmail.com>
2025-07-14 21:12:14 +0300

    Create UserService

---
+++ src/main/java/com/kaba4cow/imgxiv/auth/service/UserService.java
@@ -0,0 +1,19 @@
+package com.kaba4cow.imgxiv.auth.service;
+
+import java.util.Optional;
+
+import com.kaba4cow.imgxiv.auth.dto.AuthResponse;
+import com.kaba4cow.imgxiv.auth.dto.LoginRequest;
+import com.kaba4cow.imgxiv.auth.dto.RegisterRequest;
+import com.kaba4cow.imgxiv.auth.dto.UserDto;
+import com.kaba4cow.imgxiv.domain.user.User;
+
+public interface UserService {
+
+	UserDto register(RegisterRequest request);
+
+	AuthResponse login(LoginRequest request);
+
+	Optional<User> findByUsernameOrEmail(String usernameOrEmail);
+
+}
