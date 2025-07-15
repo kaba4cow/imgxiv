9e002726b0964c27efdf87ebf229f7329340d405
kaba4cow <kaba4cow@gmail.com>
2025-07-15 16:25:57 +0300

    Add @Schema to requests

--- src/main/java/com/kaba4cow/imgxiv/auth/dto/RegisterRequest.java
+++ src/main/java/com/kaba4cow/imgxiv/auth/dto/RegisterRequest.java
@@ -1,5 +1,6 @@
 package com.kaba4cow.imgxiv.auth.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -12,21 +13,34 @@ import lombok.Setter;
 @AllArgsConstructor
 @Getter
 @Setter
+@Schema(description = "Request for registering a new user")
 public class RegisterRequest {
 
 	@NotBlank(message = "Username is required")
 	@Size(min = 2, message = "Username is too short (min 4 characters)")
 	@Size(max = 32, message = "Username is too long (max 50 characters)")
+	@Schema(//
+			description = "Unique username", //
+			example = "john_doe"//
+	)
 	private String username;
 
 	@NotBlank(message = "Email is required")
 	@Size(max = 64, message = "Email is too long (max 64 characters)")
 	@Email(message = "Email must be valid")
+	@Schema(//
+			description = "User email", //
+			example = "john@example.com"//
+	)
 	private String email;
 
 	@NotBlank(message = "Password is required")
 	@Size(min = 8, message = "Password is too short (min 8 characters)")
 	@Size(max = 64, message = "Password is too long (max 64 characters)")
+	@Schema(//
+			description = "Password", //
+			example = "password1234"//
+	)
 	private String password;
 
 }
--- src/main/java/com/kaba4cow/imgxiv/domain/category/dto/CategoryCreateRequest.java
+++ src/main/java/com/kaba4cow/imgxiv/domain/category/dto/CategoryCreateRequest.java
@@ -1,5 +1,6 @@
 package com.kaba4cow.imgxiv.domain.category.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -11,16 +12,25 @@ import lombok.Setter;
 @AllArgsConstructor
 @Getter
 @Setter
+@Schema(description = "Request for creating a new category")
 public class CategoryCreateRequest {
 
 	@NotBlank(message = "Name is required")
 	@Size(min = 2, message = "Name is too short (min 2 characters)")
 	@Size(max = 16, message = "Name is too long (max 16 characters)")
+	@Schema(//
+			description = "Unique name", //
+			example = "category_name"//
+	)
 	private String name;
 
 	@NotBlank(message = "Description is required")
 	@Size(min = 16, message = "Description is too short (min 16 characters)")
 	@Size(max = 1024, message = "Description is too long (max 1024 characters)")
+	@Schema(//
+			description = "Description", //
+			example = "category_description"//
+	)
 	private String description;
 
 }
