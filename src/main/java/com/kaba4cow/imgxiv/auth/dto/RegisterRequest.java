2063b7ad222a3fbd68988c3ff2d3ef16a2c094e6
kaba4cow <kaba4cow@gmail.com>
2025-07-15 11:18:17 +0300

    Add RegisterRequest validation

--- src/main/java/com/kaba4cow/imgxiv/auth/dto/RegisterRequest.java
+++ src/main/java/com/kaba4cow/imgxiv/auth/dto/RegisterRequest.java
@@ -1,5 +1,8 @@
 package com.kaba4cow.imgxiv.auth.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,10 +14,16 @@ import lombok.Setter;
 @Setter
 public class RegisterRequest {
 
+	@NotBlank(message = "Username is required")
+	@Size(min = 2, max = 32, message = "Username must be between 2 and 32 characters long")
 	private String username;
 
+	@NotBlank(message = "Email is required")
+	@Email(message = "Email must be valid")
 	private String email;
 
+	@NotBlank(message = "Password is required")
+	@Size(min = 8, max = 64, message = "Password must be between 8 and 64 characters long")
 	private String password;
 
 }
