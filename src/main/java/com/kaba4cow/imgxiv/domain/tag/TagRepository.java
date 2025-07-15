27271d6b81e2170f18a39665626d2474502e74de
kaba4cow <kaba4cow@gmail.com>
2025-07-15 19:14:13 +0300

    Add existsByName to TagRepository

--- src/main/java/com/kaba4cow/imgxiv/domain/tag/TagRepository.java
+++ src/main/java/com/kaba4cow/imgxiv/domain/tag/TagRepository.java
@@ -1,7 +1,12 @@
 package com.kaba4cow.imgxiv.domain.tag;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
+
+	@Query("SELECT COUNT(t) > 0 FROM Tag t WHERE t.nameAndDescription.name = :name")
+	boolean existsByName(@Param("name") String name);
 
 }
