02dd5bf1d4f7dae23f5a155657c916147e007af2
kaba4cow <kaba4cow@gmail.com>
2025-07-15 16:07:30 +0300

    Create EntityWithIdAndCreationTimestamp

---
+++ src/main/java/com/kaba4cow/imgxiv/domain/superclass/EntityWithIdAndCreationTimestamp.java
@@ -0,0 +1,17 @@
+package com.kaba4cow.imgxiv.domain.superclass;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class EntityWithIdAndCreationTimestamp extends EntityWithId {
+
+	@CreationTimestamp
+	@Column(name = "column_created_at", updatable = false)
+	private LocalDateTime createdAt;
+
+}
