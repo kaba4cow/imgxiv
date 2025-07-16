ed3c265ae73b69734d42271bf450589bb9c059b4
kaba4cow <kaba4cow@gmail.com>
2025-07-16 13:34:01 +0300

    Add findByPostAndType

--- src/main/java/com/kaba4cow/imgxiv/domain/vote/VoteRepository.java
+++ src/main/java/com/kaba4cow/imgxiv/domain/vote/VoteRepository.java
@@ -1,7 +1,16 @@
 package com.kaba4cow.imgxiv.domain.vote;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.kaba4cow.imgxiv.domain.post.Post;
 
 public interface VoteRepository extends JpaRepository<Vote, Long> {
+
+	@Query("SELECT v FROM Vote v WHERE v.postAndUser.post = :post AND v.type = :type")
+	List<Vote> findByPostAndType(@Param("post") Post post, @Param("type") VoteType type);
 
 }
