87842b1cbc34065967f2eab993ba7642d8e93e32
kaba4cow <kaba4cow@gmail.com>
2025-07-16 18:21:16 +0300

    Remove TestInstance annotation

--- src/test/java/com/kaba4cow/imgxiv/domain/category/CategoryControllerTest.java
+++ src/test/java/com/kaba4cow/imgxiv/domain/category/CategoryControllerTest.java
@@ -6,7 +6,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,7 +13,6 @@ import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @AutoConfigureMockMvc
 @SpringBootTest
 public class CategoryControllerTest {
