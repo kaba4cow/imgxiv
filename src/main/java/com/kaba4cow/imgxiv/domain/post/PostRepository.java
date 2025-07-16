95144e21de8e4ed9e5c3e8d9b0ac55e64c766054
kaba4cow <kaba4cow@gmail.com>
2025-07-16 13:30:09 +0300

    Add findByAuthor

--- src/main/java/com/kaba4cow/imgxiv/domain/post/PostRepository.java
+++ src/main/java/com/kaba4cow/imgxiv/domain/post/PostRepository.java
@@ -1,7 +1,13 @@
 package com.kaba4cow.imgxiv.domain.post;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.kaba4cow.imgxiv.domain.user.User;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+	List<Post> findByAuthor(User author);
 
 }
