586ca91cc5a9b2d05ad4939cf60cfa47f7428075
kaba4cow <kaba4cow@gmail.com>
2025-07-16 18:18:26 +0300

    Simplify CategoryControllerTest

--- src/test/java/com/kaba4cow/imgxiv/domain/category/CategoryControllerTest.java
+++ src/test/java/com/kaba4cow/imgxiv/domain/category/CategoryControllerTest.java
@@ -5,21 +5,14 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.kaba4cow.imgxiv.domain.user.User;
-import com.kaba4cow.imgxiv.domain.user.UserRepository;
-import com.kaba4cow.imgxiv.domain.user.UserRole;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @AutoConfigureMockMvc
@@ -29,69 +22,24 @@ public class CategoryControllerTest {
 	@Autowired
 	private MockMvc mockMvc;
 
-	@Autowired
-	private UserRepository userRepository;
-
-	private String token;
-
-	@BeforeAll
-	void setup() throws Exception {
-		String reg = """
-				{
-					"username": "testuser",
-					"email": "test@example.com",
-					"password": "testpass512"
-				}
-				""";
-
-		mockMvc.perform(post("/api/auth/register")//
-				.contentType(MediaType.APPLICATION_JSON)//
-				.content(reg))//
-				.andExpect(status().isOk());
-
-		User user = userRepository.findByUsername("testuser").orElseThrow();
-		user.setRole(UserRole.MODERATOR);
-		userRepository.save(user);
-
-		String login = """
-				{
-					"usernameOrEmail": "testuser",
-					"password": "testpass512"
-				}
-				""";
-
-		MvcResult result = mockMvc.perform(post("/api/auth/login")//
-				.contentType(MediaType.APPLICATION_JSON)//
-				.content(login))//
-				.andExpect(status().isOk())//
-				.andReturn();
-
-		String json = result.getResponse().getContentAsString();
-		JsonNode node = new ObjectMapper().readTree(json);
-		token = node.get("token").asText();
-	}
-
+	@WithMockUser(authorities = "create-category")
 	@Test
 	public void createCategory() throws Exception {
-		String json = """
-				{
-					"name": "category-name",
-					"description": "category-description"
-				}
-				""";
-
 		mockMvc.perform(post("/api/categories")//
-				.header("Authorization", "Bearer " + token)//
 				.contentType(MediaType.APPLICATION_JSON)//
-				.content(json))//
+				.content("""
+							{
+								"name": "category-name",
+								"description": "category-description"
+							}
+						"""))//
 				.andExpect(status().isOk())//
 				.andExpect(jsonPath("$.name").value("category-name"));
 	}
 
 	@Test
 	public void getAllCategories() throws Exception {
-		mockMvc.perform(get("/api/categories")//
-				.header("Authorization", "Bearer " + token))//
+		mockMvc.perform(get("/api/categories"))//
 				.andExpect(status().isOk())//
 				.andExpect(jsonPath("$").isArray());
 	}
