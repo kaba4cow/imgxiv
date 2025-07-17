9dc5b15efa84cbf50ce362b2d41cd68163366ab3
kaba4cow <kaba4cow@gmail.com>
2025-07-17 16:28:34 +0300

    Reformat args

--- src/main/java/com/kaba4cow/imgxiv/auth/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/kaba4cow/imgxiv/auth/jwt/JwtAuthenticationFilter.java
@@ -36,11 +36,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 	private final UserDetailsService userDetailsService;
 
 	@Override
-	protected void doFilterInternal(//
-			HttpServletRequest request, //
-			HttpServletResponse response, //
-			FilterChain filterChain//
-	) throws ServletException, IOException {
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
 		try {
 			if (isAuthRequest(request))
 				processAuthRequest(request);
