ba113e4593231f06807dd5f58a6205bb22851bb9
kaba4cow <kaba4cow@gmail.com>
2025-07-17 22:19:52 +0300

    Add findByName()

--- src/main/java/com/kaba4cow/imgxiv/domain/tag/TagRepository.java
+++ src/main/java/com/kaba4cow/imgxiv/domain/tag/TagRepository.java
@@ -1,6 +1,7 @@
 package com.kaba4cow.imgxiv.domain.tag;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -10,6 +11,9 @@ public interface TagRepository extends JpaRepository<Tag, Long> {
 
 	@Query("SELECT COUNT(t) > 0 FROM Tag t WHERE t.nameAndDescription.name = :name")
 	boolean existsByName(@Param("name") String name);
+
+	@Query("SELECT t FROM Tag t WHERE t.nameAndDescription.name = :name")
+	Optional<Tag> findByName(String name);
 
 	List<Tag> findByCategoryId(Long categoryId);
 
