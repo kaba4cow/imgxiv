533339185fd1a97760570f5c32cac4ea1e140f46
kaba4cow <kaba4cow@gmail.com>
2025-07-17 22:33:05 +0300

    Fix DefaultPostSpecification

--- src/main/java/com/kaba4cow/imgxiv/domain/post/specification/DefaultPostSpecification.java
+++ src/main/java/com/kaba4cow/imgxiv/domain/post/specification/DefaultPostSpecification.java
@@ -9,8 +9,6 @@ import com.kaba4cow.imgxiv.domain.tag.Tag;
 
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Join;
-import jakarta.persistence.criteria.JoinType;
 import jakarta.persistence.criteria.Path;
 import jakarta.persistence.criteria.Predicate;
 import jakarta.persistence.criteria.Root;
@@ -31,27 +29,25 @@ public class DefaultPostSpecification implements PostSpecification {
 
 	@Override
 	public Predicate toPredicate(Root<Post> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
-		Join<Post, PostTag> postTags = root.join("postTags", JoinType.LEFT);
-		Join<PostTag, Tag> tag = postTags.join("tag");
 		return cb.and(//
-				createRequiredPredicate(root, query, cb), //
-				createExcludedPredicate(nameOf(tag), cb)//
+				toRequiredPredicate(root, query, cb), //
+				toExcludedPredicate(root, query, cb)//
 		);
 	}
 
-	private Predicate createRequiredPredicate(Root<Post> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
-		Predicate requiredPredicate = cb.conjunction();
+	private Predicate toRequiredPredicate(Root<Post> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
 		if (requiredTags.isEmpty())
-			return requiredPredicate;
+			return cb.conjunction();
+		Predicate requiredPredicate = cb.conjunction();
 		for (String tag : requiredTags)
 			requiredPredicate = cb.and(//
 					requiredPredicate, //
-					cb.exists(createSubquery(tag, root, query, cb))//
+					cb.exists(toRequiredSubquery(tag, root, query, cb))//
 			);
 		return requiredPredicate;
 	}
 
-	private Subquery<Long> createSubquery(String tag, Root<Post> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+	private Subquery<Long> toRequiredSubquery(String tag, Root<Post> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
 		Subquery<Long> subquery = query.subquery(Long.class);
 		Root<PostTag> postTag = subquery.from(PostTag.class);
 		return subquery.select(idOf(postTag.get("post")))//
@@ -61,10 +57,20 @@ public class DefaultPostSpecification implements PostSpecification {
 				);
 	}
 
-	private Predicate createExcludedPredicate(Path<String> tag, CriteriaBuilder cb) {
+	private Predicate toExcludedPredicate(Root<Post> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
 		return excludedTags.isEmpty()//
 				? cb.conjunction()//
-				: cb.not(tag.in(excludedTags));
+				: cb.not(cb.exists(toExcludedSubquery(root, query, cb)));
+	}
+
+	private Subquery<Long> toExcludedSubquery(Root<Post> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+		Subquery<Long> subquery = query.subquery(Long.class);
+		Root<PostTag> postTag = subquery.from(PostTag.class);
+		return subquery.select(idOf(postTag.get("post")))//
+				.where(//
+						cb.equal(idOf(postTag.get("post")), idOf(root)), //
+						nameOf(postTag.get("tag")).in(excludedTags)//
+				);
 	}
 
 	private Path<String> nameOf(Path<Tag> tag) {
