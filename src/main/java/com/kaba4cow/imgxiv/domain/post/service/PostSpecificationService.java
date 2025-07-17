288aba81b7a7005fb06730e3e1126143c0680a91
kaba4cow <kaba4cow@gmail.com>
2025-07-17 22:41:17 +0300

    Create PostSpecificationService

---
+++ src/main/java/com/kaba4cow/imgxiv/domain/post/service/PostSpecificationService.java
@@ -0,0 +1,9 @@
+package com.kaba4cow.imgxiv.domain.post.service;
+
+import com.kaba4cow.imgxiv.domain.post.specification.PostSpecification;
+
+public interface PostSpecificationService {
+
+	PostSpecification getSpecification(String query);
+
+}
