1928798012e04a1043ad6d424a357a53bab4b1ae
kaba4cow <kaba4cow@gmail.com>
2025-07-17 10:32:23 +0300

    Add postTags

--- src/main/java/com/kaba4cow/imgxiv/domain/tag/Tag.java
+++ src/main/java/com/kaba4cow/imgxiv/domain/tag/Tag.java
@@ -1,16 +1,21 @@
 package com.kaba4cow.imgxiv.domain.tag;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
 import com.kaba4cow.imgxiv.domain.base.EntityWithId;
 import com.kaba4cow.imgxiv.domain.category.Category;
 import com.kaba4cow.imgxiv.domain.embeddable.NameAndDescription;
+import com.kaba4cow.imgxiv.domain.posttag.PostTag;
 
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
@@ -30,6 +35,10 @@ public class Tag extends EntityWithId {
 
 	@Embedded
 	private NameAndDescription nameAndDescription = new NameAndDescription();
+
+	@ToString.Exclude
+	@OneToMany(mappedBy = "tag")
+	private List<PostTag> postTags = new ArrayList<>();
 
 	@ManyToOne(optional = false)
 	@JoinColumn(name = "column_category_id")
