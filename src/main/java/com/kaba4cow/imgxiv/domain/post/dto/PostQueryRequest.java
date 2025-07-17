b96d86e2c3b76693d777780d1d04a70246af49fe
kaba4cow <kaba4cow@gmail.com>
2025-07-17 23:39:48 +0300

    Add PostQueryRequest

---
+++ src/main/java/com/kaba4cow/imgxiv/domain/post/dto/PostQueryRequest.java
@@ -0,0 +1,26 @@
+package com.kaba4cow.imgxiv.domain.post.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Schema(description = "Request for querying posts")
+public class PostQueryRequest {
+
+	@NotNull(message = "Query is required")
+	@Size(max = 1024, message = "Query is too long (max 1024 characters)")
+	@Schema(//
+			description = "Query for matching posts", //
+			example = "cat pig !dog cute" //
+	)
+	private String query;
+
+}
