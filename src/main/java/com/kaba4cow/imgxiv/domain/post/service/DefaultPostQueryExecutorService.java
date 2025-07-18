3301360478c7456771e49f1e776d1d3b33b579d6
kaba4cow <kaba4cow@gmail.com>
2025-07-18 19:32:14 +0300

    Refactor DefaultPostQueryExecutorService - use PageRequestExtractor

--- src/main/java/com/kaba4cow/imgxiv/domain/post/service/DefaultPostQueryExecutorService.java
+++ src/main/java/com/kaba4cow/imgxiv/domain/post/service/DefaultPostQueryExecutorService.java
@@ -2,15 +2,12 @@ package com.kaba4cow.imgxiv.domain.post.service;
 
 import java.util.stream.Stream;
 
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import com.kaba4cow.imgxiv.common.dto.PaginationRequest;
+import com.kaba4cow.imgxiv.common.dto.PageRequestExtractor;
 import com.kaba4cow.imgxiv.domain.post.Post;
 import com.kaba4cow.imgxiv.domain.post.PostRepository;
 import com.kaba4cow.imgxiv.domain.post.dto.PostQueryRequest;
-import com.kaba4cow.imgxiv.domain.post.specification.PostSpecification;
 
 import lombok.RequiredArgsConstructor;
 
@@ -22,21 +19,14 @@ public class DefaultPostQueryExecutorService implements PostQueryExecutorService
 
 	private final PostSpecificationService postSpecificationService;
 
+	private final PageRequestExtractor pageRequestExtractor;
+
 	@Override
 	public Stream<Post> executeQuery(PostQueryRequest request) {
-		PostSpecification postSpecification = postSpecificationService.getSpecification(request.getQuery());
-		PageRequest pageRequest = getPageRequest(request);
-		return postRepository.findAll(postSpecification, pageRequest).stream();
-	}
-
-	private PageRequest getPageRequest(PostQueryRequest request) {
-		PaginationRequest pagination = request.getPagination();
-		Sort sort = getSort(request);
-		return PageRequest.of(pagination.getPage(), pagination.getSize(), sort);
-	}
-
-	private Sort getSort(PostQueryRequest request) {
-		return Sort.by(request.getSorting().getDirection(), "createdAt.timestamp");
+		return postRepository.findAll(//
+				postSpecificationService.getSpecification(request.getQuery()), //
+				pageRequestExtractor.getSortedPageRequest(request)//
+		).stream();
 	}
 
 }
