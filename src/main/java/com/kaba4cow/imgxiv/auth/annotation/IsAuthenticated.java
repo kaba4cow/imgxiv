e3fdc275104606512c21ce12830992838c8ccc84
kaba4cow <kaba4cow@gmail.com>
2025-07-18 16:26:51 +0300

    Add IsAuthenticated

---
+++ src/main/java/com/kaba4cow/imgxiv/auth/annotation/IsAuthenticated.java
@@ -0,0 +1,17 @@
+package com.kaba4cow.imgxiv.auth.annotation;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+@Documented
+@Retention(RUNTIME)
+@Target({ TYPE, METHOD })
+@PreAuthorize("isAuthenticated()")
+public @interface IsAuthenticated {}
