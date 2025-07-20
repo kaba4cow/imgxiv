175fe80ca85fef93e56ee9f7a4bacc339965682e
kaba4cow <kaba4cow@gmail.com>
2025-07-20 21:27:10 +0300

    Remove DefaultUserValidationService

--- src/main/java/com/kaba4cow/imgxiv/common/validation/UserValidationService.java
+++ src/main/java/com/kaba4cow/imgxiv/common/validation/UserValidationService.java
@@ -1,15 +1,36 @@
 package com.kaba4cow.imgxiv.common.validation;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
 import com.kaba4cow.imgxiv.common.exception.EmailConflictException;
 import com.kaba4cow.imgxiv.common.exception.PasswordMismatchException;
 import com.kaba4cow.imgxiv.common.exception.UsernameConflictException;
+import com.kaba4cow.imgxiv.domain.user.UserRepository;
 
-public interface UserValidationService {
+import lombok.RequiredArgsConstructor;
 
-	void ensureUsernameAvailable(String username) throws UsernameConflictException;
+@RequiredArgsConstructor
+@Service
+public class UserValidationService {
 
-	void ensureEmailAvailable(String email) throws EmailConflictException;
+	private final UserRepository userRepository;
 
-	void ensurePasswordsMatch(String password, String passwordHash) throws PasswordMismatchException;
+	private final PasswordEncoder passwordEncoder;
+
+	public void ensureUsernameAvailable(String username) throws UsernameConflictException {
+		if (userRepository.existsByUsername(username))
+			throw new UsernameConflictException("Username already taken");
+	}
+
+	public void ensureEmailAvailable(String email) throws EmailConflictException {
+		if (userRepository.existsByEmail(email))
+			throw new EmailConflictException("Email already taken");
+	}
+
+	public void ensurePasswordsMatch(String password, String passwordHash) throws PasswordMismatchException {
+		if (!passwordEncoder.matches(password, passwordHash))
+			throw new PasswordMismatchException("Passwords do not match");
+	}
 
 }
