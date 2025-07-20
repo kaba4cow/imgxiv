4e492edb64b2593ab6c9aabf0ffc8bbc840fc727
kaba4cow <kaba4cow@gmail.com>
2025-07-20 13:44:16 +0300

    Change PostSpecificationService getSpecification() arg to PostQuery

--- src/main/java/com/kaba4cow/imgxiv/domain/post/service/PostSpecificationService.java
+++ src/main/java/com/kaba4cow/imgxiv/domain/post/service/PostSpecificationService.java
@@ -3,9 +3,10 @@ package com.kaba4cow.imgxiv.domain.post.service;
 import org.springframework.data.jpa.domain.Specification;
 
 import com.kaba4cow.imgxiv.domain.post.Post;
+import com.kaba4cow.imgxiv.domain.post.query.PostQuery;
 
 public interface PostSpecificationService {
 
-	Specification<Post> getSpecification(String query);
+	Specification<Post> getPostSpecification(PostQuery postQuery);
 
 }
