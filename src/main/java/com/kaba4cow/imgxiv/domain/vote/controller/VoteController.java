7fd0f274b2a8ab6965e1ebb11f4a87ff85e90940
kaba4cow <kaba4cow@gmail.com>
2025-07-20 17:11:54 +0300

    Add VoteController#getVoteSummary

--- src/main/java/com/kaba4cow/imgxiv/domain/vote/controller/VoteController.java
+++ src/main/java/com/kaba4cow/imgxiv/domain/vote/controller/VoteController.java
@@ -2,6 +2,7 @@ package com.kaba4cow.imgxiv.domain.vote.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,10 +10,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kaba4cow.imgxiv.auth.annotation.IsAuthenticated;
+import com.kaba4cow.imgxiv.auth.annotation.PermitAll;
 import com.kaba4cow.imgxiv.common.controller.CurrentUserAwareController;
 import com.kaba4cow.imgxiv.domain.user.User;
 import com.kaba4cow.imgxiv.domain.vote.dto.VoteCreateRequest;
 import com.kaba4cow.imgxiv.domain.vote.dto.VoteDeleteRequest;
+import com.kaba4cow.imgxiv.domain.vote.dto.VoteRequest;
+import com.kaba4cow.imgxiv.domain.vote.dto.VoteSummaryDto;
 import com.kaba4cow.imgxiv.domain.vote.service.VoteService;
 
 import jakarta.validation.Valid;
@@ -37,6 +41,12 @@ public class VoteController extends CurrentUserAwareController {
 	public ResponseEntity<Void> deleteVote(@Valid @RequestBody VoteDeleteRequest request, @ModelAttribute User currentUser) {
 		voteService.deleteVote(request, currentUser);
 		return ResponseEntity.noContent().build();
+	}
+
+	@PermitAll
+	@GetMapping
+	public ResponseEntity<VoteSummaryDto> getVoteSummary(@Valid @RequestBody VoteRequest request) {
+		return ResponseEntity.ok(voteService.getVoteSummary(request));
 	}
 
 }
