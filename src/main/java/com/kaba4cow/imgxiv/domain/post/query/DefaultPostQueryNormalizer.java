f756a0a1d46457bceadcc2b63acecf8f7384b035
kaba4cow <kaba4cow@gmail.com>
2025-07-20 14:20:10 +0300

    Move query splitting logic to DefaultPostQueryNormalizer

--- src/main/java/com/kaba4cow/imgxiv/domain/post/query/DefaultPostQueryNormalizer.java
+++ src/main/java/com/kaba4cow/imgxiv/domain/post/query/DefaultPostQueryNormalizer.java
@@ -1,16 +1,49 @@
 package com.kaba4cow.imgxiv.domain.post.query;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RequiredArgsConstructor
 @Component
 public class DefaultPostQueryNormalizer implements PostQueryNormalizer {
 
 	@Override
-	public String normalizeQuery(String query) {
-		return query.toLowerCase();
+	public NormalizedPostQuery normalizeQuery(String query) {
+		QueryBuilder builder = new QueryBuilder();
+		List<String> tags = splitQuery(query);
+		for (String tag : tags)
+			builder.addTag(tag);
+		NormalizedPostQuery postQuery = builder.build();
+		log.info("Normalized query '{}' to {}", query, postQuery);
+		return postQuery;
+	}
+
+	private List<String> splitQuery(String query) {
+		return Arrays.asList(query//
+				.toLowerCase()//
+				.split("\\s+")//
+		);
+	}
+
+	private static class QueryBuilder {
+
+		private final List<String> tags = new ArrayList<>();
+
+		private void addTag(String tag) {
+			tags.add(tag);
+		}
+
+		private NormalizedPostQuery build() {
+			return new NormalizedPostQuery(tags);
+		}
+
 	}
 
 }
