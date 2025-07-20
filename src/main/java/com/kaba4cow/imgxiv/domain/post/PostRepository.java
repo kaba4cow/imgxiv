db8f3e85b60e54d29f6ae536b5cfd35547a0cba5
kaba4cow <kaba4cow@gmail.com>
2025-07-20 22:53:19 +0300

    Removed ExtendedJpaRepository

--- src/main/java/com/kaba4cow/imgxiv/domain/post/PostRepository.java
+++ src/main/java/com/kaba4cow/imgxiv/domain/post/PostRepository.java
@@ -2,13 +2,18 @@ package com.kaba4cow.imgxiv.domain.post;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-import com.kaba4cow.imgxiv.common.repository.ExtendedJpaRepository;
+import com.kaba4cow.imgxiv.common.exception.NotFoundException;
 import com.kaba4cow.imgxiv.domain.user.User;
 
-public interface PostRepository extends ExtendedJpaRepository<Post, Long>, JpaSpecificationExecutor<Post> {
+public interface PostRepository extends JpaRepository<Post, Long>, JpaSpecificationExecutor<Post> {
 
 	List<Post> findByAuthor(User author);
+
+	default Post findByIdOrThrow(Long id) {
+		return findById(id).orElseThrow(() -> new NotFoundException(String.format("Post not found: %s", id)));
+	}
 
 }
--- src/main/java/com/kaba4cow/imgxiv/domain/tag/TagRepository.java
+++ src/main/java/com/kaba4cow/imgxiv/domain/tag/TagRepository.java
@@ -1,15 +1,19 @@
 package com.kaba4cow.imgxiv.domain.tag;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import com.kaba4cow.imgxiv.common.repository.ExtendedJpaRepository;
+import com.kaba4cow.imgxiv.common.exception.NotFoundException;
 import com.kaba4cow.imgxiv.domain.category.Category;
 
-public interface TagRepository extends ExtendedJpaRepository<Tag, Long> {
+public interface TagRepository extends JpaRepository<Tag, Long> {
 
 	@Query("SELECT COUNT(t) > 0 FROM Tag t WHERE t.nameAndDescription.name = :name")
 	boolean existsByName(@Param("name") String name);
@@ -18,5 +22,16 @@ public interface TagRepository extends ExtendedJpaRepository<Tag, Long> {
 	Optional<Tag> findByName(String name);
 
 	List<Tag> findByCategory(Category category);
+
+	default Tag findByIdOrThrow(Long id) {
+		return findById(id).orElseThrow(() -> new NotFoundException(String.format("Tag not found: %s", id)));
+	}
+
+	default Set<Tag> findByIdsOrThrow(Collection<? extends Long> ids) {
+		return ids.stream()//
+				.distinct()//
+				.map(this::findByIdOrThrow)//
+				.collect(Collectors.toSet());
+	}
 
 }
