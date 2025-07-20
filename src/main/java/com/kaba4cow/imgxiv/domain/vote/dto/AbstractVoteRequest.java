37db7dee367f9d69e32be5e7c52db7f6e9e3a1c0
kaba4cow <kaba4cow@gmail.com>
2025-07-20 15:30:03 +0300

    Remove AbstractVoteRequest#AllArgsConstructor

--- src/main/java/com/kaba4cow/imgxiv/domain/vote/dto/AbstractVoteRequest.java
+++ src/main/java/com/kaba4cow/imgxiv/domain/vote/dto/AbstractVoteRequest.java
@@ -2,13 +2,11 @@ package com.kaba4cow.imgxiv.domain.vote.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @NoArgsConstructor
-@AllArgsConstructor
 @Getter
 @Setter
 public abstract class AbstractVoteRequest {
