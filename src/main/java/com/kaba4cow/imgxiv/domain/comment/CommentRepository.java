fa2d93126f56f6f04a4211e431e8bfa0a58ede43
kaba4cow <kaba4cow@gmail.com>
2025-07-20 22:25:44 +0300

    Extend CommentRepository from ExtendedJpaRepository

--- src/main/java/com/kaba4cow/imgxiv/domain/comment/CommentRepository.java
+++ src/main/java/com/kaba4cow/imgxiv/domain/comment/CommentRepository.java
@@ -2,14 +2,14 @@ package com.kaba4cow.imgxiv.domain.comment;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import com.kaba4cow.imgxiv.common.repository.ExtendedJpaRepository;
 import com.kaba4cow.imgxiv.domain.post.Post;
 import com.kaba4cow.imgxiv.domain.user.User;
 
-public interface CommentRepository extends JpaRepository<Comment, Long> {
+public interface CommentRepository extends ExtendedJpaRepository<Comment, Long> {
 
 	@Query("SELECT c FROM Comment c WHERE c.postAndUser.post = :post")
 	List<Comment> findByPost(@Param("post") Post post);
