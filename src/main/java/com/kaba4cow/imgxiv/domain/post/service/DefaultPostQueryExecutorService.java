92ba52af1b87efeb5b3dcff533d462c7808a438f
kaba4cow <kaba4cow@gmail.com>
2025-07-20 13:45:04 +0300

    Fix DefaultPostQueryExecutorService

--- src/main/java/com/kaba4cow/imgxiv/domain/post/service/DefaultPostQueryExecutorService.java
+++ src/main/java/com/kaba4cow/imgxiv/domain/post/service/DefaultPostQueryExecutorService.java
@@ -8,6 +8,7 @@ import com.kaba4cow.imgxiv.common.dto.PageRequestExtractor;
 import com.kaba4cow.imgxiv.domain.post.Post;
 import com.kaba4cow.imgxiv.domain.post.PostRepository;
 import com.kaba4cow.imgxiv.domain.post.dto.PostQueryRequest;
+import com.kaba4cow.imgxiv.domain.post.query.PostQuery;
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,14 +18,17 @@ public class DefaultPostQueryExecutorService implements PostQueryExecutorService
 
 	private final PostRepository postRepository;
 
+	private final PostQueryService postQueryService;
+
 	private final PostSpecificationService postSpecificationService;
 
 	private final PageRequestExtractor pageRequestExtractor;
 
 	@Override
 	public Stream<Post> executeQuery(PostQueryRequest request) {
+		PostQuery postQuery = postQueryService.getPostQuery(request.getQuery());
 		return postRepository.findAll(//
-				postSpecificationService.getSpecification(request.getQuery()), //
+				postSpecificationService.getPostSpecification(postQuery), //
 				pageRequestExtractor.getPageRequest(request, "createdAt.timestamp")//
 		).stream();
 	}
