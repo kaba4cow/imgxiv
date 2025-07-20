53c5fd94924042ce2c2b770faf27cb995d941620
kaba4cow <kaba4cow@gmail.com>
2025-07-20 16:53:03 +0300

    Add VoteService#getVoteSummary

--- src/main/java/com/kaba4cow/imgxiv/domain/vote/service/VoteService.java
+++ src/main/java/com/kaba4cow/imgxiv/domain/vote/service/VoteService.java
@@ -1,13 +1,17 @@
 package com.kaba4cow.imgxiv.domain.vote.service;
 
+import com.kaba4cow.imgxiv.domain.post.Post;
 import com.kaba4cow.imgxiv.domain.user.User;
 import com.kaba4cow.imgxiv.domain.vote.dto.VoteCreateRequest;
 import com.kaba4cow.imgxiv.domain.vote.dto.VoteDeleteRequest;
+import com.kaba4cow.imgxiv.domain.vote.dto.VoteSummaryDto;
 
 public interface VoteService {
 
 	void createVote(VoteCreateRequest request, User user);
 
 	void deleteVote(VoteDeleteRequest request, User user);
+
+	VoteSummaryDto getVoteSummary(Post post);
 
 }
