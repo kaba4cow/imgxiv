2928e0347f0bdb41dc3aa12d6e8ca62d93801858
kaba4cow <kaba4cow@gmail.com>
2025-07-20 22:54:21 +0300

    Removed CommentRepository extension from ExtendedJpaRepository

--- src/main/java/com/kaba4cow/imgxiv/domain/comment/CommentRepository.java
+++ src/main/java/com/kaba4cow/imgxiv/domain/comment/CommentRepository.java
@@ -2,19 +2,24 @@ package com.kaba4cow.imgxiv.domain.comment;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import com.kaba4cow.imgxiv.common.repository.ExtendedJpaRepository;
+import com.kaba4cow.imgxiv.common.exception.NotFoundException;
 import com.kaba4cow.imgxiv.domain.post.Post;
 import com.kaba4cow.imgxiv.domain.user.User;
 
-public interface CommentRepository extends ExtendedJpaRepository<Comment, Long> {
+public interface CommentRepository extends JpaRepository<Comment, Long> {
 
 	@Query("SELECT c FROM Comment c WHERE c.postAndUser.post = :post")
 	List<Comment> findByPost(@Param("post") Post post);
 
 	@Query("SELECT c FROM Comment c WHERE c.postAndUser.user = :user")
 	List<Comment> findByUser(@Param("user") User user);
+
+	default Comment findByIdOrThrow(Long id) {
+		return findById(id).orElseThrow(() -> new NotFoundException(String.format("Comment not found: %s", id)));
+	}
 
 }
