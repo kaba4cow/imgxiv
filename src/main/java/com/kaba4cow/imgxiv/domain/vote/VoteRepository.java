077cd2cbfd6d1b353c6d7cdf68385f457c513a7d
kaba4cow <kaba4cow@gmail.com>
2025-07-20 16:49:46 +0300

    Add VoteRepository#getVoteSummary

--- src/main/java/com/kaba4cow/imgxiv/domain/vote/VoteRepository.java
+++ src/main/java/com/kaba4cow/imgxiv/domain/vote/VoteRepository.java
@@ -1,7 +1,5 @@
 package com.kaba4cow.imgxiv.domain.vote;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,7 +8,12 @@ import com.kaba4cow.imgxiv.domain.post.Post;
 
 public interface VoteRepository extends JpaRepository<Vote, VoteId> {
 
-	@Query("SELECT v FROM Vote v WHERE v.post = :post AND v.type = :type")
-	List<Vote> findByPostAndType(@Param("post") Post post, @Param("type") VoteType type);
+	@Query("""
+			SELECT
+				SUM(CASE WHEN v.type = 'UP' THEN 1 ELSE 0 END) AS upVoteCount,
+				SUM(CASE WHEN v.type = 'DOWN' THEN 1 ELSE 0 END) AS downVoteCount
+			FROM Vote v WHERE v.post = :post
+			""")
+	VoteSummary getVoteSummary(@Param("post") Post post);
 
 }
---
+++ src/main/java/com/kaba4cow/imgxiv/domain/vote/VoteSummary.java
@@ -0,0 +1,9 @@
+package com.kaba4cow.imgxiv.domain.vote;
+
+public interface VoteSummary {
+
+	long getUpVoteCount();
+
+	long getDownVoteCount();
+
+}
