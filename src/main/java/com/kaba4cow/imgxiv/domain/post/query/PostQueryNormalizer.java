487d69637baec7f1a9981c777b5dbe04f76b2a16
kaba4cow <kaba4cow@gmail.com>
2025-07-20 14:19:55 +0300

    Change PostQueryNormalizer to return NormalizedPostQuery

--- src/main/java/com/kaba4cow/imgxiv/domain/post/query/PostQueryNormalizer.java
+++ src/main/java/com/kaba4cow/imgxiv/domain/post/query/PostQueryNormalizer.java
@@ -2,6 +2,6 @@ package com.kaba4cow.imgxiv.domain.post.query;
 
 public interface PostQueryNormalizer {
 
-	String normalizeQuery(String query);
+	NormalizedPostQuery normalizeQuery(String query);
 
 }
