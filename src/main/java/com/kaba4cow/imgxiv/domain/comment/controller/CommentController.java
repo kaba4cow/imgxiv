840db73087354ad83fa3783fc21a715a3f009e93
kaba4cow <kaba4cow@gmail.com>
2025-07-20 21:53:44 +0300

    Add CommentController#getCommentsByPost

--- src/main/java/com/kaba4cow/imgxiv/domain/comment/controller/CommentController.java
+++ src/main/java/com/kaba4cow/imgxiv/domain/comment/controller/CommentController.java
@@ -1,23 +1,29 @@
 package com.kaba4cow.imgxiv.domain.comment.controller;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kaba4cow.imgxiv.auth.annotation.IsAuthenticated;
+import com.kaba4cow.imgxiv.auth.annotation.PermitAll;
 import com.kaba4cow.imgxiv.common.controller.CurrentUserAwareController;
 import com.kaba4cow.imgxiv.domain.comment.dto.CommentCreateRequest;
-import com.kaba4cow.imgxiv.domain.comment.dto.CommentDeleteRequest;
 import com.kaba4cow.imgxiv.domain.comment.dto.CommentDto;
 import com.kaba4cow.imgxiv.domain.comment.dto.CommentEditRequest;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -50,12 +56,32 @@ public class CommentController extends CurrentUserAwareController {
 	}
 
 	@Operation(//
-			summary = "Deletes comment", //
+			summary = "Delete comment", //
 			description = "Deletes specified comment"//
 	)
 	@IsAuthenticated
 	@DeleteMapping
-	public ResponseEntity<CommentDto> deleteComment(@Valid @RequestBody CommentDeleteRequest request) {
+	public ResponseEntity<CommentDto> deleteComment(//
+			@Schema(//
+					description = "ID of the comment", //
+					example = "1"//
+			) //
+			@RequestParam("id") @NotNull Long id) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Operation(//
+			summary = "Get comments of specified post", //
+			description = "Retrieves comments of specified post"//
+	)
+	@PermitAll
+	@GetMapping
+	public ResponseEntity<List<CommentDto>> getCommentsByPost(//
+			@Schema(//
+					description = "ID of the post", //
+					example = "1"//
+			) //
+			@RequestParam("postId") @NotNull Long postId) {
 		throw new UnsupportedOperationException();
 	}
 
