a7c7aa901ff210745ffa51f14f92e787c7754093
kaba4cow <kaba4cow@gmail.com>
2025-07-20 15:25:31 +0300

    Change VoteRepository ID key to VoteId

--- src/main/java/com/kaba4cow/imgxiv/domain/vote/VoteRepository.java
+++ src/main/java/com/kaba4cow/imgxiv/domain/vote/VoteRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.repository.query.Param;
 
 import com.kaba4cow.imgxiv.domain.post.Post;
 
-public interface VoteRepository extends JpaRepository<Vote, Long> {
+public interface VoteRepository extends JpaRepository<Vote, VoteId> {
 
 	@Query("SELECT v FROM Vote v WHERE v.postAndUser.post = :post AND v.type = :type")
 	List<Vote> findByPostAndType(@Param("post") Post post, @Param("type") VoteType type);
