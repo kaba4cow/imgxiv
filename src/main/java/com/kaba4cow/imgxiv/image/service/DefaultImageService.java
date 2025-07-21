5373b8c4bae4b5fe1a75f37b2644407bd522fd3b
kaba4cow <kaba4cow@gmail.com>
2025-07-21 20:24:01 +0300

    Create DefaultImageService

---
+++ src/main/java/com/kaba4cow/imgxiv/image/service/DefaultImageService.java
@@ -0,0 +1,37 @@
+package com.kaba4cow.imgxiv.image.service;
+
+import java.io.InputStream;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.kaba4cow.imgxiv.domain.embeddable.PostImage;
+import com.kaba4cow.imgxiv.image.storage.ImageStorage;
+import com.kaba4cow.imgxiv.image.storage.StorageKeyGenerator;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class DefaultImageService implements ImageService {
+
+	private final ImageStorage imageStorage;
+
+	private final StorageKeyGenerator storageKeyGenerator;
+
+	@Override
+	public void uploadImage(MultipartFile file) {
+		imageStorage.uploadImage(storageKeyGenerator.generateKey(file.getOriginalFilename()), file);
+	}
+
+	@Override
+	public InputStream getImage(PostImage postImage) {
+		return imageStorage.getImage(postImage.getStorageKey());
+	}
+
+	@Override
+	public void deleteImage(PostImage postImage) {
+		imageStorage.deleteImage(postImage.getStorageKey());
+	}
+
+}
