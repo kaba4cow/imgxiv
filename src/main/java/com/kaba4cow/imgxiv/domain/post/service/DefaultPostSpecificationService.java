9a04cfd820cbb7e3c931876c9159433d808594d7
kaba4cow <kaba4cow@gmail.com>
2025-07-21 12:27:46 +0300

    Add DefaultPostSpecificationService

---
+++ src/main/java/com/kaba4cow/imgxiv/domain/post/service/DefaultPostSpecificationService.java
@@ -0,0 +1,22 @@
+package com.kaba4cow.imgxiv.domain.post.service;
+
+import org.springframework.stereotype.Service;
+
+import com.kaba4cow.imgxiv.domain.post.query.CompiledPostQuery;
+import com.kaba4cow.imgxiv.domain.post.specification.PostSpecification;
+import com.kaba4cow.imgxiv.domain.post.specification.PostSpecificationFactory;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class DefaultPostSpecificationService implements PostSpecificationService {
+
+	private final PostSpecificationFactory postSpecificationFactory;
+
+	@Override
+	public PostSpecification getPostSpecification(CompiledPostQuery compiledQuery) {
+		return postSpecificationFactory.createPostSpecification(compiledQuery);
+	}
+
+}
