2939c3915b825917dc3a48f6a189c6d0797f8f4f
kaba4cow <kaba4cow@gmail.com>
2025-07-21 13:29:12 +0300

    Add PostSecurity

---
+++ src/main/java/com/kaba4cow/imgxiv/domain/post/security/PostSecurity.java
@@ -0,0 +1,27 @@
+package com.kaba4cow.imgxiv.domain.post.security;
+
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.stereotype.Component;
+
+import com.kaba4cow.imgxiv.domain.post.Post;
+import com.kaba4cow.imgxiv.domain.post.PostRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Component
+public class PostSecurity {
+
+	private final PostRepository postRepository;
+
+	@PostAuthorize("returnObject.author.id == principal.id")
+	public Post getPostToEdit(Long id) {
+		return postRepository.findByIdOrThrow(id);
+	}
+
+	@PostAuthorize("returnObject.author.id == principal.id or hasRole('moderator')")
+	public Post getPostToDelete(Long id) {
+		return postRepository.findByIdOrThrow(id);
+	}
+
+}
