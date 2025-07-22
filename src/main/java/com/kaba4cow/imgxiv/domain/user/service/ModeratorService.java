1aaca811b3859d3ccf2d6ae9d0a331aa3fec371f
kaba4cow <kaba4cow@gmail.com>
2025-07-22 18:33:16 +0300

    Add ModeratorService#getModerators

--- src/main/java/com/kaba4cow/imgxiv/domain/user/service/ModeratorService.java
+++ src/main/java/com/kaba4cow/imgxiv/domain/user/service/ModeratorService.java
@@ -1,6 +1,13 @@
 package com.kaba4cow.imgxiv.domain.user.service;
 
+import java.util.List;
+
+import com.kaba4cow.imgxiv.common.dto.PageableRequest;
+import com.kaba4cow.imgxiv.domain.user.dto.UserDto;
+
 public interface ModeratorService {
+
+	List<UserDto> getModerators(PageableRequest request);
 
 	void assignModerator(Long id);
 
