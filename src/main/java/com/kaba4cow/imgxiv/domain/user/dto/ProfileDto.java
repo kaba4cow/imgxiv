8696a154d4e9682731e5476f762aafeac94b7aaa
kaba4cow <kaba4cow@gmail.com>
2025-07-22 17:26:00 +0300

    Create ProfileDto

---
+++ src/main/java/com/kaba4cow/imgxiv/domain/user/dto/ProfileDto.java
@@ -0,0 +1,20 @@
+package com.kaba4cow.imgxiv.domain.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class ProfileDto {
+
+	private Long id;
+
+	private String username;
+
+	private String email;
+
+}
