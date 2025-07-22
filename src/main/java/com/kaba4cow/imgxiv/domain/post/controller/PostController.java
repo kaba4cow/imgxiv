b9fefa81062432fdd598201bd8c8bfd340c2e5d3
kaba4cow <kaba4cow@gmail.com>
2025-07-22 08:01:16 +0300

    Move PostController api docs to interface

--- src/main/java/com/kaba4cow/imgxiv/domain/post/controller/PostController.java
+++ src/main/java/com/kaba4cow/imgxiv/domain/post/controller/PostController.java
@@ -5,21 +5,9 @@ import java.util.concurrent.TimeUnit;
 
 import org.springframework.core.io.Resource;
 import org.springframework.http.CacheControl;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.kaba4cow.imgxiv.auth.annotation.CurrentUser;
-import com.kaba4cow.imgxiv.auth.annotation.IsAuthenticated;
-import com.kaba4cow.imgxiv.auth.annotation.PermitAll;
 import com.kaba4cow.imgxiv.domain.post.dto.PostCreateRequest;
 import com.kaba4cow.imgxiv.domain.post.dto.PostDto;
 import com.kaba4cow.imgxiv.domain.post.dto.PostEditRequest;
@@ -28,46 +16,21 @@ import com.kaba4cow.imgxiv.domain.post.service.PostService;
 import com.kaba4cow.imgxiv.domain.user.User;
 import com.kaba4cow.imgxiv.image.ImageResource;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-@Tag(//
-		name = "Posts", //
-		description = "Endpoints for post creation and retrieval"//
-)
-@RequestMapping("/api/posts")
 @RestController
-public class PostController {
+public class PostController implements PostControllerApiDoc {
 
 	private final PostService postService;
 
-	@Operation(//
-			summary = "Create a new post", //
-			description = "Creates a new post with the provided tags and returns the post data"//
-	)
-	@IsAuthenticated
-	@PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-	public ResponseEntity<PostDto> createPost(@Valid @ModelAttribute PostCreateRequest request, @CurrentUser User user) {
+	@Override
+	public ResponseEntity<PostDto> createPost(PostCreateRequest request, User user) {
 		return ResponseEntity.ok(postService.createPost(request, user));
 	}
 
-	@Operation(//
-			summary = "Load post image", //
-			description = "Loads image of the specified post"//
-	)
-	@GetMapping("/image")
-	public ResponseEntity<Resource> getPostImage(//
-			@NotNull(message = "Post ID is required") //
-			@Schema(//
-					description = "ID of the post", //
-					example = "1"//
-			) //
-			@RequestParam("id") Long id) {
+	@Override
+	public ResponseEntity<Resource> getPostImage(Long id) {
 		ImageResource image = postService.getPostImage(id);
 		return ResponseEntity.ok()//
 				.cacheControl(CacheControl.maxAge(1L, TimeUnit.HOURS).cachePublic())//
@@ -76,40 +39,19 @@ public class PostController {
 				.body(image);
 	}
 
-	@Operation(//
-			summary = "Edit post", //
-			description = "Edits specified post"//
-	)
-	@IsAuthenticated
-	@PatchMapping
-	public ResponseEntity<PostDto> editPost(@Valid @RequestBody PostEditRequest request) {
+	@Override
+	public ResponseEntity<PostDto> editPost(PostEditRequest request) {
 		return ResponseEntity.ok(postService.editPost(request));
 	}
 
-	@Operation(//
-			summary = "Delete post", //
-			description = "Deletes specified post"//
-	)
-	@IsAuthenticated
-	@DeleteMapping
-	public ResponseEntity<Void> deletePost(//
-			@NotNull(message = "Post ID is required") //
-			@Schema(//
-					description = "ID of the post", //
-					example = "1"//
-			) //
-			@RequestParam("id") Long id) {
+	@Override
+	public ResponseEntity<Void> deletePost(Long id) {
 		postService.deletePost(id);
 		return ResponseEntity.noContent().build();
 	}
 
-	@Operation(//
-			summary = "Search posts by tag query", //
-			description = "Performs a tag-based search and returns a list of post data"//
-	)
-	@PermitAll
-	@PostMapping("/search")
-	public ResponseEntity<List<PostDto>> searchPosts(@Valid @RequestBody PostQueryRequest request) {
+	@Override
+	public ResponseEntity<List<PostDto>> searchPosts(PostQueryRequest request) {
 		return ResponseEntity.ok(postService.findPostsByQuery(request));
 	}
 
---
+++ src/main/java/com/kaba4cow/imgxiv/domain/post/controller/PostControllerApiDoc.java
@@ -0,0 +1,131 @@
+package com.kaba4cow.imgxiv.domain.post.controller;
+
+import java.util.List;
+
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.kaba4cow.imgxiv.auth.annotation.CurrentUser;
+import com.kaba4cow.imgxiv.auth.annotation.IsAuthenticated;
+import com.kaba4cow.imgxiv.auth.annotation.PermitAll;
+import com.kaba4cow.imgxiv.domain.post.dto.PostCreateRequest;
+import com.kaba4cow.imgxiv.domain.post.dto.PostDto;
+import com.kaba4cow.imgxiv.domain.post.dto.PostEditRequest;
+import com.kaba4cow.imgxiv.domain.post.dto.PostQueryRequest;
+import com.kaba4cow.imgxiv.domain.user.User;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+
+@Tag(//
+		name = "Posts", //
+		description = """
+				Endpoints for post creation and retrieval.
+				"""//
+)
+@RequestMapping("/api/posts")
+public interface PostControllerApiDoc {
+
+	@Operation(//
+			summary = "Create a new post", //
+			description = """
+					Creates a new post with the provided tags and returns the post data.
+					"""//
+	)
+	@IsAuthenticated
+	@PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	ResponseEntity<PostDto> createPost(//
+
+			@Valid //
+			@ModelAttribute //
+			PostCreateRequest request, //
+
+			@CurrentUser //
+			User user//
+
+	);
+
+	@Operation(//
+			summary = "Load post image", //
+			description = """
+					Loads image of the specified post
+					"""//
+	)
+	@GetMapping("/image")
+	ResponseEntity<Resource> getPostImage(//
+
+			@NotNull(message = "Post ID is required") //
+			@Schema(//
+					description = "ID of the post", //
+					example = "1"//
+			) //
+			@RequestParam("id") //
+			Long id//
+
+	);
+
+	@Operation(//
+			summary = "Edit post", //
+			description = """
+					Edits specified post.
+					"""//
+	)
+	@IsAuthenticated
+	@PatchMapping
+	ResponseEntity<PostDto> editPost(//
+
+			@Valid //
+			@RequestBody //
+			PostEditRequest request//
+
+	);
+
+	@Operation(//
+			summary = "Delete post", //
+			description = """
+					Deletes specified post.
+					"""//
+	)
+	@IsAuthenticated
+	@DeleteMapping
+	ResponseEntity<Void> deletePost(//
+
+			@NotNull(message = "Post ID is required") //
+			@Schema(//
+					description = "ID of the post", //
+					example = "1"//
+			) //
+			@RequestParam("id") //
+			Long id//
+
+	);
+
+	@Operation(//
+			summary = "Search posts by tag query", //
+			description = """
+					Performs a tag-based search and returns a list of post data.
+					"""//
+	)
+	@PermitAll
+	@PostMapping("/search")
+	ResponseEntity<List<PostDto>> searchPosts(//
+
+			@Valid //
+			@RequestBody //
+			PostQueryRequest request//
+
+	);
+
+}
