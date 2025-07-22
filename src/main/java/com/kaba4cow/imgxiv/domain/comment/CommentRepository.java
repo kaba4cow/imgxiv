93f60c56f697d933bbce0b9b1cb7010bbdd75481
kaba4cow <kaba4cow@gmail.com>
2025-07-22 21:26:41 +0300

    Remove `@Query` from findByX methods, add countByX methods

--- src/main/java/com/kaba4cow/imgxiv/domain/comment/CommentRepository.java
+++ src/main/java/com/kaba4cow/imgxiv/domain/comment/CommentRepository.java
@@ -3,8 +3,6 @@ package com.kaba4cow.imgxiv.domain.comment;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import com.kaba4cow.imgxiv.common.exception.NotFoundException;
 import com.kaba4cow.imgxiv.domain.post.Post;
@@ -12,11 +10,13 @@ import com.kaba4cow.imgxiv.domain.user.User;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-	@Query("SELECT c FROM Comment c WHERE c.post = :post")
-	List<Comment> findByPost(@Param("post") Post post);
+	List<Comment> findByPost(Post post);
 
-	@Query("SELECT c FROM Comment c WHERE c.author = :author")
-	List<Comment> findByAuthor(@Param("author") User author);
+	long countByPost(Post post);
+
+	List<Comment> findByAuthor(User author);
+
+	long countByAuthor(User author);
 
 	default Comment findByIdOrThrow(Long id) {
 		return findById(id).orElseThrow(() -> new NotFoundException("Comment", id));
