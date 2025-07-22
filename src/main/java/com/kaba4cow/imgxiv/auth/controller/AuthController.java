e488414f897027c00687123bd4af8bd72301597b
kaba4cow <kaba4cow@gmail.com>
2025-07-22 08:00:56 +0300

    Move AuthController api docs to interface

--- src/main/java/com/kaba4cow/imgxiv/auth/controller/AuthController.java
+++ src/main/java/com/kaba4cow/imgxiv/auth/controller/AuthController.java
@@ -1,51 +1,29 @@
 package com.kaba4cow.imgxiv.auth.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.kaba4cow.imgxiv.auth.annotation.PermitAll;
 import com.kaba4cow.imgxiv.auth.dto.AuthResponse;
 import com.kaba4cow.imgxiv.auth.dto.LoginRequest;
 import com.kaba4cow.imgxiv.auth.dto.RegisterRequest;
 import com.kaba4cow.imgxiv.auth.service.UserAuthService;
 import com.kaba4cow.imgxiv.domain.user.dto.UserDto;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-@Tag(//
-		name = "Authentication", //
-		description = "User registration and login endpoints"//
-)
-@RequestMapping("/api/auth")
 @RestController
-public class AuthController {
+public class AuthController implements AuthControllerApiDoc {
 
 	private final UserAuthService userAuthService;
 
-	@Operation(//
-			summary = "Register new user", //
-			description = "Creates a new user account and returns basic user information"//
-	)
-	@PermitAll
-	@PostMapping("/register")
-	public ResponseEntity<UserDto> registerUser(@RequestBody @Valid RegisterRequest request) {
+	@Override
+	public ResponseEntity<UserDto> registerUser(RegisterRequest request) {
 		return ResponseEntity.ok(userAuthService.register(request));
 	}
 
-	@Operation(//
-			summary = "Authenticate user and return token", //
-			description = "Authenticates the provided credentials and returns a JWT token along with user info"//
-	)
-	@PermitAll
-	@PostMapping("/login")
-	public ResponseEntity<AuthResponse> authenticateUser(@RequestBody LoginRequest request) {
+	@Override
+	public ResponseEntity<AuthResponse> authenticateUser(LoginRequest request) {
 		return ResponseEntity.ok(userAuthService.login(request));
 	}
 
---
+++ src/main/java/com/kaba4cow/imgxiv/auth/controller/AuthControllerApiDoc.java
@@ -0,0 +1,59 @@
+package com.kaba4cow.imgxiv.auth.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kaba4cow.imgxiv.auth.annotation.PermitAll;
+import com.kaba4cow.imgxiv.auth.dto.AuthResponse;
+import com.kaba4cow.imgxiv.auth.dto.LoginRequest;
+import com.kaba4cow.imgxiv.auth.dto.RegisterRequest;
+import com.kaba4cow.imgxiv.domain.user.dto.UserDto;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+
+@Tag(//
+		name = "Authentication", //
+		description = """
+				User registration and login endpoints.
+				"""//
+)
+@RequestMapping("/api/auth")
+public interface AuthControllerApiDoc {
+
+	@Operation(//
+			summary = "Register new user", //
+			description = """
+					Creates a new user account and returns basic user information.
+					"""//
+	)
+	@PermitAll
+	@PostMapping("/register")
+	public ResponseEntity<UserDto> registerUser(//
+
+			@Valid //
+			@RequestBody //
+			RegisterRequest request//
+
+	);
+
+	@Operation(//
+			summary = "Authenticate user and return token", //
+			description = """
+					Authenticates the provided credentials and returns a JWT token along with user info.
+					"""//
+	)
+	@PermitAll
+	@PostMapping("/login")
+	public ResponseEntity<AuthResponse> authenticateUser(//
+
+			@Valid //
+			@RequestBody //
+			LoginRequest request//
+
+	);
+
+}
