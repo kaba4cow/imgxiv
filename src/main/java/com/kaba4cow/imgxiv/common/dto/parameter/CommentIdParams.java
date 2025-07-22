09de4a3a1f55919e4e54def23b8cc0c7545e15d0
kaba4cow <kaba4cow@gmail.com>
2025-07-22 08:07:05 +0300

    Create CommentIdParams

---
+++ src/main/java/com/kaba4cow/imgxiv/common/dto/parameter/CommentIdParams.java
@@ -0,0 +1,21 @@
+package com.kaba4cow.imgxiv.common.dto.parameter;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class CommentIdParams {
+
+	@NotNull(message = "Comment ID is required") //
+	@Schema(//
+			description = "ID of the comment", //
+			example = "1"//
+	) //
+	private Long id;
+
+}
