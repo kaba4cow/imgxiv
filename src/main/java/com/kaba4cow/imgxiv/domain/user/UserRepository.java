837b0cef38ff2b3a6daa6760de30ef1936308819
kaba4cow <kaba4cow@gmail.com>
2025-07-22 18:41:27 +0300

    Add UserRepository#findAllByRole

--- src/main/java/com/kaba4cow/imgxiv/domain/user/UserRepository.java
+++ src/main/java/com/kaba4cow/imgxiv/domain/user/UserRepository.java
@@ -2,6 +2,8 @@ package com.kaba4cow.imgxiv.domain.user;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,6 +11,8 @@ import org.springframework.data.repository.query.Param;
 import com.kaba4cow.imgxiv.common.exception.NotFoundException;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+	Page<User> findAllByRole(UserRole role, Pageable pageable);
 
 	default User findByIdOrThrow(Long id) {
 		return findById(id).orElseThrow(() -> new NotFoundException(String.format("User not found: %s", id)));
