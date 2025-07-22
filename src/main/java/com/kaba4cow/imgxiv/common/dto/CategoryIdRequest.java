ba5e706124336f07fa738652f8a5afaf65a65da2
kaba4cow <kaba4cow@gmail.com>
2025-07-22 08:34:32 +0300

    Add setters to ID requests

--- src/main/java/com/kaba4cow/imgxiv/common/dto/CategoryIdRequest.java
+++ src/main/java/com/kaba4cow/imgxiv/common/dto/CategoryIdRequest.java
@@ -2,13 +2,13 @@ package com.kaba4cow.imgxiv.common.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @NoArgsConstructor
-@AllArgsConstructor
 @Getter
+@Setter
 public class CategoryIdRequest {
 
 	@NotNull(message = "Category ID is required") //
