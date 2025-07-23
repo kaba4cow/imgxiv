b9201cf33f2dc2004175383b1e497f17e76a7b5b
kaba4cow <kaba4cow@gmail.com>
2025-07-23 21:41:26 +0300

    Add ConditionalOnPropertyEnable

---
+++ src/main/java/com/kaba4cow/imgxiv/common/condition/OnPropertyEnableCondition.java
@@ -0,0 +1,29 @@
+package com.kaba4cow.imgxiv.common.condition;
+
+import java.util.Map;
+import java.util.Objects;
+
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+
+public class OnPropertyEnableCondition implements Condition {
+
+	@Override
+	public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+		Map<String, Object> attributes = metadata.getAnnotationAttributes(ConditionalOnPropertyEnable.class.getName());
+		if (Objects.isNull(attributes))
+			return false;
+
+		String prefix = (String) attributes.get("prefix");
+		boolean matchIfMissing = (Boolean) attributes.get("matchIfMissing");
+
+		String key = "%s.enable".concat(prefix);
+		String value = context.getEnvironment().getProperty(key);
+
+		return Objects.isNull(value)//
+				? matchIfMissing//
+				: value.equalsIgnoreCase("true");
+	}
+
+}
