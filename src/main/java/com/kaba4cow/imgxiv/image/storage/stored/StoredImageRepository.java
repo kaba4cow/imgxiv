aaa2b3beb802840d495947a2662dabea7f4c855f
kaba4cow <kaba4cow@gmail.com>
2025-07-23 23:16:14 +0300

    Add Lazy annotation

--- src/main/java/com/kaba4cow/imgxiv/image/storage/stored/StoredImageRepository.java
+++ src/main/java/com/kaba4cow/imgxiv/image/storage/stored/StoredImageRepository.java
@@ -1,9 +1,11 @@
 package com.kaba4cow.imgxiv.image.storage.stored;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.kaba4cow.imgxiv.common.exception.NotFoundException;
 
+@Lazy
 public interface StoredImageRepository extends JpaRepository<StoredImage, String> {
 
 	default StoredImage findByIdOrThrow(String id) {
