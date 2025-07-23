c8ca6c3e944d73d6702ebd0eec9ac3e6398591ac
kaba4cow <kaba4cow@gmail.com>
2025-07-23 09:12:04 +0300

    Create UserRoles and UserAuthorities

---
+++ src/main/java/com/kaba4cow/imgxiv/domain/user/UserAuthorities.java
@@ -0,0 +1,25 @@
+package com.kaba4cow.imgxiv.domain.user;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class UserAuthorities {
+
+	public static final String CREATE_POST = "create-post";
+
+	public static final String CREATE_CATEGORY = "create-category";
+
+	public static final String CREATE_TAG = "create-tag";
+
+	public static final String EDIT_POST_NA = "edit-post-na";
+
+	public static final String DELETE_POST_NA = "delete-post-na";
+
+	public static final String DELETE_COMMENT_NA = "delete-comment-na";
+
+	public static final String VIEW_MODERATORS = "view-moderators";
+
+	public static final String MANAGE_MODERATORS = "manage-moderators";
+
+}
