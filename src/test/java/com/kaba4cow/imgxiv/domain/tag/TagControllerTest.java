32e746665b8efc6dc45fed441050177d6316ae7c
kaba4cow <kaba4cow@gmail.com>
2025-07-24 12:18:33 +0300

    Rewrite TagController test

--- src/test/java/com/kaba4cow/imgxiv/domain/tag/TagControllerTest.java
+++ src/test/java/com/kaba4cow/imgxiv/domain/tag/TagControllerTest.java
@@ -2,7 +2,6 @@ package com.kaba4cow.imgxiv.domain.tag;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -12,15 +11,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.kaba4cow.imgxiv.domain.category.Category;
 import com.kaba4cow.imgxiv.domain.category.CategoryRepository;
-import com.kaba4cow.imgxiv.domain.user.UserAuthorities;
+import com.kaba4cow.imgxiv.domain.category.service.CategoryService;
 
 import lombok.SneakyThrows;
 
@@ -36,17 +33,20 @@ public class TagControllerTest {
 	private CategoryRepository categoryRepository;
 
 	@Autowired
+	private CategoryService categoryService;
+
+	@Autowired
 	private TagRepository tagRepository;
 
 	@SneakyThrows
-	@WithMockUser(authorities = UserAuthorities.CREATE_TAG)
 	@Test
-	public void createsTagWithAuthority() {
+	public void retrievesTag() {
+		Category category = saveTestCategory("name");
 		String name = "name";
 		String description = "description";
-		Long categoryId = saveTestCategory("name").getId();
+		Tag tag = saveTestTag(name, description, category);
 
-		performCreateTag(name, description, categoryId)//
+		performGetTag(tag.getId())//
 				.andExpect(status().isOk())//
 				.andExpect(jsonPath("$.id").isNumber())//
 				.andExpect(jsonPath("$.name").isString())//
@@ -54,50 +54,18 @@ public class TagControllerTest {
 				.andExpect(jsonPath("$.description").isString())//
 				.andExpect(jsonPath("$.description").value(description))//
 				.andExpect(jsonPath("$.categoryId").isNumber())//
-				.andExpect(jsonPath("$.categoryId").value(categoryId));
+				.andExpect(jsonPath("$.categoryId").value(category.getId()));
+	}
+
+	@SneakyThrows
+	private ResultActions performGetTag(Long tagId) {
+		return mockMvc.perform(get("/api/tags")//
+				.param("tagId", tagId.toString()));
 	}
 
 	@SneakyThrows
 	@Test
-	public void doesNotCreateTagWithoutAuthority() {
-		performCreateTag("name", "", saveTestCategory("name").getId())//
-				.andExpect(status().is4xxClientError())//
-				.andExpect(status().isForbidden());
-	}
-
-	@SneakyThrows
-	@WithMockUser(authorities = UserAuthorities.CREATE_TAG)
-	@Test
-	public void doesNotCreateTagWithInvalidName() {
-		performCreateTag("[tag.]?", "", saveTestCategory("category").getId())//
-				.andExpect(status().is4xxClientError())//
-				.andExpect(status().isBadRequest());
-	}
-
-	@SneakyThrows
-	@WithMockUser(authorities = UserAuthorities.CREATE_TAG)
-	@Test
-	public void doesNotCreateTagWithTakenName() {
-		Category category = saveTestCategory("category");
-		String name = "name";
-		saveTestTag(name, category);
-		performCreateTag(name, "", category.getId())//
-				.andExpect(status().is4xxClientError())//
-				.andExpect(status().isConflict());
-	}
-
-	@SneakyThrows
-	@WithMockUser(authorities = UserAuthorities.CREATE_TAG)
-	@Test
-	public void doesNotCreateTagWithCategoryNotFound() {
-		performCreateTag("name", "", 0l)//
-				.andExpect(status().is4xxClientError())//
-				.andExpect(status().isNotFound());
-	}
-
-	@SneakyThrows
-	@Test
-	public void retrievesTagsByCategory() {
+	public void retrievesCategorizedTags() {
 		Map<Category, Integer> categoriesWithNumbersOfTags = Map.of(//
 				saveTestCategory("category1"), 3, //
 				saveTestCategory("category2"), 5, //
@@ -108,60 +76,11 @@ public class TagControllerTest {
 		for (Map.Entry<Category, Integer> entry : categoriesWithNumbersOfTags.entrySet()) {
 			Category category = entry.getKey();
 			int numberOfTags = entry.getValue();
-			performGetTagsByCategory(category.getId())//
+			performGetCategorizedTags(category.getId())//
 					.andExpect(status().isOk())//
 					.andExpect(jsonPath("$").isArray())//
 					.andExpect(jsonPath("$", hasSize(numberOfTags)));
 		}
-	}
-
-	@SneakyThrows
-	@Test
-	public void retrievesAllTags() {
-		int numberOfTags = 5;
-		generateTagsForCategories(Map.of(saveTestCategory("name"), numberOfTags));
-
-		performGetAllTags()//
-				.andExpect(status().isOk())//
-				.andExpect(jsonPath("$").isArray())//
-				.andExpect(jsonPath("$", hasSize(numberOfTags)));
-	}
-
-	@SneakyThrows
-	@Test
-	public void retrievesNoTags() {
-		performGetAllTags()//
-				.andExpect(status().isOk())//
-				.andExpect(jsonPath("$").isArray())//
-				.andExpect(jsonPath("$", hasSize(0)));
-	}
-
-	@SneakyThrows
-	private ResultActions performCreateTag(String name, String description, Long categoryId) {
-		return mockMvc.perform(post("/api/tags")//
-				.contentType(MediaType.APPLICATION_JSON)//
-				.content("""
-							{
-								"name": "%s",
-								"description": "%s",
-								"categoryId": %s
-							}
-						""".formatted(//
-						name, //
-						description, //
-						categoryId//
-				)));
-	}
-
-	@SneakyThrows
-	private ResultActions performGetTagsByCategory(Long categoryId) {
-		return mockMvc.perform(get("/api/tags")//
-				.param("categoryId", categoryId.toString()));
-	}
-
-	@SneakyThrows
-	private ResultActions performGetAllTags() {
-		return mockMvc.perform(get("/api/tags/all"));
 	}
 
 	private void generateTagsForCategories(Map<Category, Integer> categoriesWithNumbersOfTags) {
@@ -169,15 +88,42 @@ public class TagControllerTest {
 			Category category = entry.getKey();
 			int numberOfTags = entry.getValue();
 			for (int i = 0; i < numberOfTags; i++)
-				saveTestTag(category.getNameAndDescription().getName() + "_tag" + i, category);
+				saveTestTag(category.getNameAndDescription().getName() + "_tag" + i, "description", category);
 		}
 	}
 
-	private Tag saveTestTag(String name, Category category) {
+	@SneakyThrows
+	private ResultActions performGetCategorizedTags(Long categoryId) {
+		return mockMvc.perform(get("/api/tags/categorized")//
+				.param("categoryId", categoryId.toString()));
+	}
+
+	@SneakyThrows
+	@Test
+	public void retrievesUncategorizedTags() {
+		int numberOfTags = 10;
+		for (int i = 0; i < numberOfTags; i++)
+			saveTestTag("tag" + i, "description");
+		performGetUncategorizedTags()//
+				.andExpect(status().isOk())//
+				.andExpect(jsonPath("$").isArray())//
+				.andExpect(jsonPath("$", hasSize(numberOfTags)));
+	}
+
+	@SneakyThrows
+	private ResultActions performGetUncategorizedTags() {
+		return mockMvc.perform(get("/api/tags/uncategorized"));
+	}
+
+	private Tag saveTestTag(String name, String description) {
+		return saveTestTag(name, description, categoryService.getDefaultCategory());
+	}
+
+	private Tag saveTestTag(String name, String description, Category category) {
 		Tag tag = new Tag();
 		tag.setCategory(category);
 		tag.getNameAndDescription().setName(name);
-		tag.getNameAndDescription().setDescription("description");
+		tag.getNameAndDescription().setDescription(description);
 		return tagRepository.saveAndFlush(tag);
 	}
 
