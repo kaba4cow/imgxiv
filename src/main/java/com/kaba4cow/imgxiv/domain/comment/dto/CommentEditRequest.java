b6a9ad999ded2079f009a1fe9ce7359c1e8c586d
kaba4cow <kaba4cow@gmail.com>
2025-07-24 19:07:23 +0300

    Remove id

--- src/main/java/com/kaba4cow/imgxiv/domain/comment/dto/CommentEditRequest.java
+++ src/main/java/com/kaba4cow/imgxiv/domain/comment/dto/CommentEditRequest.java
@@ -2,7 +2,6 @@ package com.kaba4cow.imgxiv.domain.comment.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -15,13 +14,6 @@ import lombok.Setter;
 @Setter
 @Schema(description = "Request for editing a comment")
 public class CommentEditRequest {
-
-	@NotNull(message = "Comment ID is required")
-	@Schema(//
-			description = "ID of the comment", //
-			example = "1"//
-	)
-	private Long id;
 
 	@NotBlank(message = "Comment text is required")
 	@Size(max = 1024, message = "Text is too long (max 1024 characters)")
