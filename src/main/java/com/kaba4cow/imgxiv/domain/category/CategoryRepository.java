c4aafb9b71b309602de5381c21a15bca921b74e5
kaba4cow <kaba4cow@gmail.com>
2025-07-24 11:33:34 +0300

    Add findByName

--- src/main/java/com/kaba4cow/imgxiv/domain/category/CategoryRepository.java
+++ src/main/java/com/kaba4cow/imgxiv/domain/category/CategoryRepository.java
@@ -1,5 +1,7 @@
 package com.kaba4cow.imgxiv.domain.category;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,6 +12,9 @@ public interface CategoryRepository extends JpaRepository<Category, Long> {
 
 	@Query("SELECT COUNT(c) > 0 FROM Category c WHERE c.nameAndDescription.name = :name")
 	boolean existsByName(@Param("name") String name);
+
+	@Query("SELECT c FROM Category c WHERE c.nameAndDescription.name = :name")
+	Optional<Category> findByName(String name);
 
 	default Category findByIdOrThrow(Long id) {
 		return findById(id).orElseThrow(() -> new NotFoundException("Category", id));
