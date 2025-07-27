ff40a539de0da55477379f5e154a884688dccfd7
kaba4cow <kaba4cow@gmail.com>
2025-07-27 23:57:35 +0300

    Remove PostQueryNormalizer usage

--- src/main/java/com/kaba4cow/imgxiv/domain/post/service/DefaultPostQueryExecutorService.java
+++ src/main/java/com/kaba4cow/imgxiv/domain/post/service/DefaultPostQueryExecutorService.java
@@ -9,8 +9,6 @@ import com.kaba4cow.imgxiv.domain.post.Post;
 import com.kaba4cow.imgxiv.domain.post.PostRepository;
 import com.kaba4cow.imgxiv.domain.post.dto.PostQueryRequest;
 import com.kaba4cow.imgxiv.domain.post.query.CompiledPostQuery;
-import com.kaba4cow.imgxiv.domain.post.query.NormalizedPostQuery;
-import com.kaba4cow.imgxiv.domain.post.query.PostQueryNormalizer;
 import com.kaba4cow.imgxiv.domain.post.service.query.PostQueryService;
 import com.kaba4cow.imgxiv.domain.post.service.specification.PostSpecificationService;
 import com.kaba4cow.imgxiv.domain.post.specification.PostSpecification;
@@ -23,21 +21,15 @@ public class DefaultPostQueryExecutorService implements PostQueryExecutorService
 
 	private final PostRepository postRepository;
 
-	private final PostQueryNormalizer postQueryNormalizer;
-
 	private final PostQueryService postQueryService;
 
 	private final PostSpecificationService postSpecificationService;
 
 	@Override
 	public Stream<Post> executeQuery(PostQueryRequest request, Pageable pageable) {
-		NormalizedPostQuery normalizedQuery = postQueryNormalizer.normalizeQuery(request.getQuery());
-		CompiledPostQuery compiledQuery = postQueryService.getCompiledQuery(normalizedQuery);
-		PostSpecification postSpecification = postSpecificationService.getPostSpecification(compiledQuery);
-		return postRepository.findAll(//
-				postSpecification, //
-				pageable//
-		).stream();
+		CompiledPostQuery postQuery = postQueryService.getCompiledQuery(request.getQuery());
+		PostSpecification postSpecification = postSpecificationService.getPostSpecification(postQuery);
+		return postRepository.findAll(postSpecification, pageable).stream();
 	}
 
 }
