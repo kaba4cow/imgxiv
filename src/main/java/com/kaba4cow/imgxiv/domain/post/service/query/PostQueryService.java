e783d6f14c501b154d37c5d234d0dfc7289e255d
kaba4cow <kaba4cow@gmail.com>
2025-07-28 00:16:07 +0300

    Remove PostQueryService

--- src/main/java/com/kaba4cow/imgxiv/domain/post/service/query/PostQueryService.java
+++
@@ -1,9 +0,0 @@
-package com.kaba4cow.imgxiv.domain.post.service.query;
-
-import com.kaba4cow.imgxiv.domain.post.query.PostQuery;
-
-public interface PostQueryService {
-
-	PostQuery getCompiledQuery(String query);
-
-}
