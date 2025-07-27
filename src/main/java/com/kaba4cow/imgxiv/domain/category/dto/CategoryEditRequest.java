03958a8265cba1ea7752e4fdde38b59d2330c5aa
kaba4cow <kaba4cow@gmail.com>
2025-07-27 18:18:24 +0300

    Create CategoryEditRequest

---
+++ src/main/java/com/kaba4cow/imgxiv/domain/category/dto/CategoryEditRequest.java
@@ -0,0 +1,33 @@
+package com.kaba4cow.imgxiv.domain.category.dto;
+
+import com.kaba4cow.imgxiv.common.validation.CategoryName;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Schema(description = "Request for editing a category")
+public class CategoryEditRequest {
+
+	@CategoryName
+	@Schema(//
+			description = "New category name", //
+			example = "category_name"//
+	)
+	private String name;
+
+	@Size(max = 1024, message = "Description is too long (max 1024 characters)")
+	@Schema(//
+			description = "New description", //
+			example = "Category description"//
+	)
+	private String description = "";
+
+}
