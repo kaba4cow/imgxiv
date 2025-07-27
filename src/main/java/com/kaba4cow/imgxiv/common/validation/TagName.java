8b9072a9cb65a234941b26425c136b8b8311342d
kaba4cow <kaba4cow@gmail.com>
2025-07-27 13:11:50 +0300

    Create TagName annotation

---
+++ src/main/java/com/kaba4cow/imgxiv/common/validation/TagName.java
@@ -0,0 +1,34 @@
+package com.kaba4cow.imgxiv.common.validation;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE_USE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+@Documented
+@Retention(RUNTIME)
+@Target({ FIELD, PARAMETER, TYPE_USE })
+@Constraint(validatedBy = {})
+@NotBlank(message = "Tag name is required")
+@Size(min = 2, message = "Tag name is too short (min 2 characters)")
+@Size(max = 32, message = "Tag name is too long (max 32 characters)")
+@Pattern(regexp = "^[a-zA-Z0-9_]*$", message = "Tag name must contain only letters, digits and underscores")
+public @interface TagName {
+
+	String message() default "Invalid tag name";
+
+	Class<?>[] groups() default {};
+
+	Class<? extends Payload>[] payload() default {};
+
+}
