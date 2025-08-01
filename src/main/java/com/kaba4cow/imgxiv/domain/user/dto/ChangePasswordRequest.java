1e3e3f39062cd9c2cb2b258cc5a967889b753739
kaba4cow <kaba4cow@gmail.com>
2025-08-01 23:29:52 +0300

    Replace constraints with custom ones

--- src/main/java/com/kaba4cow/imgxiv/domain/user/dto/ChangePasswordRequest.java
+++ src/main/java/com/kaba4cow/imgxiv/domain/user/dto/ChangePasswordRequest.java
@@ -1,8 +1,9 @@
 package com.kaba4cow.imgxiv.domain.user.dto;
 
+import com.kaba4cow.imgxiv.domain.user.validation.ValidPassword;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,8 +23,7 @@ public class ChangePasswordRequest {
 	private String oldPassword;
 
 	@NotBlank(message = "New password is required")
-	@Size(min = 8, message = "Password is too short (min 8 characters)")
-	@Size(max = 64, message = "Password is too long (max 64 characters)")
+	@ValidPassword
 	@Schema(//
 			description = "New password", //
 			example = "password1234_new"//
